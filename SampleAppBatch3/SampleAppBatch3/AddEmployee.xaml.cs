2df79bbd6cd61f4c4393ad7017b457b6bc9de7c1
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-08 15:49:52 +0700

    menambahkan data di sqlite

--- SampleAppBatch3/SampleAppBatch3/AddEmployee.xaml.cs
+++ SampleAppBatch3/SampleAppBatch3/AddEmployee.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SampleAppBatch3.DAL;
+using SampleAppBatch3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +14,33 @@ namespace SampleAppBatch3
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddEmployee : ContentPage
     {
+        private DataAccess _dataAccess;
         public AddEmployee()
         {
             InitializeComponent();
+            _dataAccess = new DataAccess();
         }
 
-        private void btnSave_Clicked(object sender, EventArgs e)
+        private async void btnSave_Clicked(object sender, EventArgs e)
         {
-
+            var newEmp = new Employee
+            {
+                EmpName = txtEmpName.Text,
+                Designation = txtDesignation.Text,
+                Department = txtDepartment.Text,
+                Qualification = txtQualification.Text
+            };
+            try
+            {
+                var result = _dataAccess.InsertEmployee(newEmp);
+                await DisplayAlert("Keterangan", $"Data Emp {newEmp.EmpName} berhasil ditambah",
+                    "OK");
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Kesalahan", ex.Message, "OK");
+            }
         }
     }
 }
