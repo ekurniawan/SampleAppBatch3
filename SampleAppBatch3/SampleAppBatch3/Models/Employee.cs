9c18b450ebe2b2542df86b9f46d58763af6e87f5
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-08 14:18:50 +0700

    menambahkan class employee

---
+++ SampleAppBatch3/SampleAppBatch3/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleAppBatch3.Models
+{
+    public class Employee
+    {
+        [PrimaryKey,AutoIncrement]
+        public int EmpId { get; set; }
+
+        [NotNull]
+        public string EmpName { get; set; }
+
+        public string Designation { get; set; }
+        public string Department { get; set; }
+        public string Qualification { get; set; }
+    }
+
+    
+}
