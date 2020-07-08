bd9e689418e1130aa77d20721013780e6f90eca1
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-08 14:47:57 +0700

    menambahkan DataAccess

--- SampleAppBatch3/SampleAppBatch3/DAL/DataAccess.cs
+++ SampleAppBatch3/SampleAppBatch3/DAL/DataAccess.cs
@@ -3,6 +3,7 @@ using SQLite;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Xamarin.Essentials;
 
@@ -24,6 +25,22 @@ namespace SampleAppBatch3.DAL
         {
             _conn = GetConn();
             _conn.CreateTable<Employee>();
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            //return _conn.Query<Employee>("select * from Employee order by EmpName asc");
+            //var results = _conn.Table<Employee>().OrderBy(e => e.EmpName);
+            var result = from e in _conn.Table<Employee>()
+                         orderby e.EmpName
+                         select e;
+
+            return result;
+        }
+
+        public int InsertEmployee(Employee employee)
+        {
+            var result = 
         }
     }
 }
