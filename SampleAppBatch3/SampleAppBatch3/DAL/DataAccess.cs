0fa39affba548520c7c370f404416564766beb02
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-08 14:30:54 +0700

    menambahkan class data access

---
+++ SampleAppBatch3/SampleAppBatch3/DAL/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using SampleAppBatch3.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace SampleAppBatch3.DAL
+{
+    public class DataAccess
+    {
+        private SQLiteConnection _conn;
+        public SQLiteConnection GetConn()
+        {
+            SQLiteConnection conn;
+            var dbName = "MyDb.db3";
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, dbName);
+            conn = new SQLiteConnection(dbPath);
+            return conn;
+        }
+
+        public DataAccess()
+        {
+            _conn = GetConn();
+            _conn.CreateTable<Employee>();
+        }
+    }
+}
