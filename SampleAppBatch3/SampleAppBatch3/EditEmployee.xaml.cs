b8379fd78dc1531b37c6b8264f9713e3d526185d
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-09 09:47:00 +0700

    edit data sqlite

--- SampleAppBatch3/SampleAppBatch3/EditEmployee.xaml.cs
+++ SampleAppBatch3/SampleAppBatch3/EditEmployee.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SampleAppBatch3.DAL;
+using SampleAppBatch3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,40 @@ namespace SampleAppBatch3
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EditEmployee : ContentPage
     {
+        private DataAccess _dataAccess;
         public EditEmployee()
         {
             InitializeComponent();
+            _dataAccess = new DataAccess();
+        }
+
+        private async void btnEdit_Clicked(object sender, EventArgs e)
+        {
+            var editData = new Employee
+            {
+                EmpId = Convert.ToInt32(txtEmpID.Text),
+                EmpName = txtEmpName.Text,
+                Designation = txtDesignation.Text,
+                Department = txtDepartment.Text,
+                Qualification = txtQualification.Text
+            };
+            try
+            {
+                var result = _dataAccess.EditEmployee(editData);
+                if(result!=1)
+                {
+                    await DisplayAlert("Error", "Gagal Update Data", "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Keterangan", $"Update Emp {editData.EmpName} berhasil", "OK");
+                    await Navigation.PopAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
         }
     }
 }
