c55cd67332cd5379eade06d0dda5d8cd2fd770c7
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-09 10:10:59 +0700

    menambahkan context action

--- SampleAppBatch3/SampleAppBatch3/ShowEmployee.xaml.cs
+++ SampleAppBatch3/SampleAppBatch3/ShowEmployee.xaml.cs
@@ -39,5 +39,29 @@ namespace SampleAppBatch3
             frmEditEmployee.BindingContext = selectedItem;
             await Navigation.PushAsync(frmEditEmployee);
         }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var myMenu = (MenuItem)sender;
+            var empId = Convert.ToInt32(myMenu.CommandParameter);
+            try
+            {
+                var konfirmasi = await DisplayAlert("Konfirmasi", "Yakin delete data?", "Yes", "No");
+                if (konfirmasi)
+                {
+                    var delEmp = new Employee
+                    {
+                        EmpId = empId
+                    };
+                    var result = _dataAccess.DeleteEmployee(delEmp);
+                    await DisplayAlert("Keterangan", "Data berhasil di delete", "OK");
+                    OnAppearing();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
     }
 }
