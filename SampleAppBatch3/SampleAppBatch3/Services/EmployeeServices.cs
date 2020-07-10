ccb96cca970583ff1997d74ab64b3d8f5b6b1aac
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-10 14:34:54 +0700

    menambahkan edit data

--- SampleAppBatch3/SampleAppBatch3/Services/EmployeeServices.cs
+++ SampleAppBatch3/SampleAppBatch3/Services/EmployeeServices.cs
@@ -59,5 +59,23 @@ namespace SampleAppBatch3.Services
                 throw new Exception(ex.Message);
             }
         }
+
+        public async Task Edit(Employee emp)
+        {
+            var uri = new Uri($"{Helpers.restUrl}/api/Employee");
+            try
+            {
+                var jsonData = JsonConvert.SerializeObject(emp);
+                var content = new StringContent(jsonData, Encoding.UTF8,
+                    "application/json");
+                var response = await _client.PutAsync(uri, content);
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception("Gagal mengupdate data");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
